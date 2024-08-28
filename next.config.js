a9404742abb64c1805529e49b7063d00d0e00fe4
Guilhaof <luisphelipe1000@gmail.com>
2024-08-28 03:01:25 -0300

    remove domain next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'c4.wallpaperflare.com',
-
-      }
-    ]
-  }
 }
 module.exports = nextConfig;
